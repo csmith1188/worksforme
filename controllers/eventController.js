81e680336badd47efeb9150b60b425e8259887ce
Tyler Morgan <102384418+TYLERMARTIN1234@users.noreply.github.com>
2025-01-31 14:57:35 -0500

    current events branch

---
+++ controllers/eventController.js
@@ -0,0 +1,8 @@
+const jwt = require('jsonwebtoken');
+const urlHelper = require('../util/urlHelper.js');
+
+const eventService = require('../services/eventService.js');
+
+
+module.exports = {
+}
---
+++ services/eventService.js
@@ -0,0 +1,8 @@
+const path = require('path');
+const sql = require('sqlite3').verbose();
+const db = require('../util/dbAsyncWrapper');
+
+//db stuff here
+
+module.exports = {
+}
