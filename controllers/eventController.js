b6111c27842011a73e230884adcaba76d38814fd
Tyler Morgan <102384418+TYLERMARTIN1234@users.noreply.github.com>
2025-02-04 14:47:19 -0500

    creating events
    makes the event appear in the database
    issue #1 checkbox

--- controllers/eventController.js
+++ controllers/eventController.js
@@ -2,7 +2,8 @@ const jwt = require('jsonwebtoken');
 const urlHelper = require('../util/urlHelper.js');
 
 const eventService = require('../services/eventService.js');
-
+const express = require('express');
+const router = express.Router();
 
 module.exports = {
 }
--- routes/eventRoutes.js
+++ routes/eventRoutes.js
@@ -1,18 +1,25 @@
 const express = require('express');
 const router = express.Router();
+const db = require('../util/dbAsyncWrapper');
 const eventController = require('../controllers/eventController');
 const auth = require('../middleware/auth');
 
-router.get('/calender', auth, async(req, res) => {
+router.get('/calendar', auth, async (req, res) => {
     res.render('pages/event');
 });
 
-router.get('/createEvent', auth, async(req, res) => {
+router.get('/createEvent', auth, async (req, res) => {
     res.render('pages/createEvent');
 });
 
-router.post('/createEvent', auth, async(req, res) => {
+router.post('/createEvent', auth, async (req, res) => {
+    const { name, description } = req.body;
+    const sql = 'INSERT INTO events (name, description) VALUES (?, ?)';
+    const params = [name, description];
     
-}); 
+    await db.run(sql, params);
+    res.redirect('/event/calendar');
+
+});
 
 module.exports = router;
