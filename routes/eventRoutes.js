5a2f029623fa13b8d91ebcf3eb0b57c6c6f2a0ee
Tyler Morgan <102384418+TYLERMARTIN1234@users.noreply.github.com>
2025-02-05 09:13:57 -0500

    updates to the layout
    change the layout a bit

--- routes/eventRoutes.js
+++ routes/eventRoutes.js
@@ -4,18 +4,19 @@ const db = require('../util/dbAsyncWrapper');
 const eventController = require('../controllers/eventController');
 const auth = require('../middleware/auth');
 
-router.get('/calendar', auth, async (req, res) => {
+router.get('/calendar/:uid', auth, async (req, res) => {
     res.render('pages/event');
 });
 
 router.get('/createEvent', auth, async (req, res) => {
     res.render('pages/createEvent');
 });
-
+//temporary code
+//move to eventController and eventroutes later -chicken sandwich
 router.post('/createEvent', auth, async (req, res) => {
-    const { name, description } = req.body;
-    const sql = 'INSERT INTO events (name, description) VALUES (?, ?)';
-    const params = [name, description];
+    const { uid, name, description } = req.body;
+    const sql = 'INSERT INTO events (uid, name, description) VALUES (?, ?, ?)';
+    const params = [uid, name, description];
     
     await db.run(sql, params);
     res.redirect('/event/calendar');
