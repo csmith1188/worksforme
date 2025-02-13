79d55a12c300029649f554eb388660550090281a
ChickenSandwich <102384418+TYLERMARTIN1234@users.noreply.github.com>
2025-02-13 05:56:52 -0500

    different event pages complete
    adds different pages for each event (currently VERY barebones) issue #4 event page

--- routes/eventRoutes.js
+++ routes/eventRoutes.js
@@ -5,22 +5,38 @@ const eventController = require('../controllers/eventController');
 const auth = require('../middleware/auth');
 
 router.get('/calendar', auth, async (req, res) => {
-    const { uid, name, description,  } = req.body;
     const sql = 'SELECT * FROM events';
     let rows = await db.all(sql);
-    console.log(rows);
     res.render('pages/event', { events: rows });
 });
 
 router.get('/createEvent', auth, async (req, res) => {
     res.render('pages/createEvent');
 });
+
+router.get('/eventPage/:aEvent', auth, async (req, res) => {
+    const aEvent = req.params.aEvent;
+    const sql = 'SELECT * FROM events WHERE uid = ?';
+    const event = await db.get(sql, [aEvent]);
+
+    if (!event) {
+        return res.status(404).send('Event not found');
+    }
+
+    res.render('pages/eventPage');
+});
+
+
 //temporary code
 //move to eventController and eventroutes later -chicken sandwich
+router.post('/eventPage/:aEvent', (req, res) => {
+})
+
+
 router.post('/createEvent', auth, async (req, res) => {
     const { uid, name, description } = req.body;
-    const sql = 'INSERT INTO events (uid, name, description, creator) VALUES (?, ?, ?, ?)';
-    const params = [uid, name, description, creator];
+    const sql = 'INSERT INTO events (uid, name, description) VALUES (?, ?, ?)';
+    const params = [uid, name, description];
     
     await db.run(sql, params);
     res.redirect('/event/calendar');
