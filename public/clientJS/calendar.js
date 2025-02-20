63a0207cf13aaf60cebc5a12253979daeb6230e6
hayde26904 <hayde26904@live.ytech.edu>
2025-02-20 15:01:29 -0500

    trying my best

--- public/clientJS/calendar.js
+++ public/clientJS/calendar.js
@@ -1,36 +1,34 @@
 const pxPer15Mins = 10;
 
 let isMouseDown = false;
-let draggingBlock = null;
+let draggingElement = null;
 
 let grid;
 
 document.addEventListener('DOMContentLoaded', function() {
     grid = document.getElementById('grid');
 
-    document.addEventListener('mousedown', function(event) {
+    grid.addEventListener('mousedown', function(e) {
         isMouseDown = true;
-    
-        if (event.target.classList.contains('time-block')) {
-            draggingBlock = event.target;
-        }
+        draggingElement = e.target;
     });
-    
-    document.addEventListener('mouseup', function(event) {
+
+    grid.addEventListener('mouseup', function(e) {
         isMouseDown = false;
-        if (draggingBlock) {
-            draggingBlock = null;
+        draggingElement = null;
+    });
+
+    grid.addEventListener('mousemove', function(e) {
+        if (!isMouseDown) return;
+
+        const target = e.target;
+
+        if(target.classList.contains('cell')){
+            
+        } else if(target.classList.contains('time-block')){
+
         }
+
     });
     
-    document.addEventListener('mousemove', function(event) {
-        if (draggingBlock) {
-            let rect = draggingBlock.getBoundingClientRect();
-            let x = (event.clientX - rect.width / 2) - parseInt(grid.style.left);
-            let y = (event.clientY - rect.height / 2) - parseInt(grid.style.top);
-    
-            draggingBlock.style.left = x + 'px';
-            draggingBlock.style.top = y + 'px';
-        }
-    });
 });
