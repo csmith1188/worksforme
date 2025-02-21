64af7f812f9b5dec407473f2a479cebd39aa26a8
hayde26904 <hayde26904@live.ytech.edu>
2025-02-21 11:13:17 -0500

    BLOCKS! THEY RESIZE! (not upward yet)

--- public/clientJS/calendar.js
+++ public/clientJS/calendar.js
@@ -1,31 +1,79 @@
-const pxPer15Mins = 10;
+// the .25 accounts for the border of the cell
+// biggest brain blast of my entire life
+const pxPer15Mins = 10.25;
 
 let isMouseDown = false;
 let draggingElement = null;
+let newBlock = null;
+let targetCell = null;
 
 let grid;
+
+function snapNum(num, snapTo) {
+    return Math.floor(num / snapTo) * snapTo;
+}
 
 document.addEventListener('DOMContentLoaded', function() {
     grid = document.getElementById('grid');
 
-    grid.addEventListener('mousedown', function(e) {
+    document.addEventListener('mousedown', function(e) {
         isMouseDown = true;
-        draggingElement = e.target;
+
+        if (e.target.classList.contains('inner-cell')) {
+            targetCell = e.target.parentElement;
+            newBlock = document.createElement('div');
+            newBlock.classList.add('time-block');
+
+            let cellBox = targetCell.getBoundingClientRect();
+            // diff between mouseY and the cellY
+            let yDiff = e.clientY - cellBox.top;
+            // round to the nearest 15 minutes
+            let blockY = snapNum(yDiff, pxPer15Mins);
+
+            newBlock.style.top = blockY + 'px';
+
+        }
+
+
     });
 
-    grid.addEventListener('mouseup', function(e) {
+    document.addEventListener('mouseup', function(e) {
         isMouseDown = false;
-        draggingElement = null;
+
+        if (newBlock) {
+            // add the block to the cell if the user releases the mouse
+            if(!newBlock.parentElement){
+                targetCell.appendChild(newBlock);
+            }
+
+            newBlock = null;
+            targetCell = null;
+
+        }
     });
 
-    grid.addEventListener('mousemove', function(e) {
-        if (!isMouseDown) return;
+    document.addEventListener('mousemove', function(e) {
 
-        const target = e.target;
+        if(newBlock){
+            // if the block hasn't been added to the cell, add it
+            if(!newBlock.parentElement){
+                targetCell.appendChild(newBlock);
+            }
 
-        if(target.classList.contains('cell')){
-            
-        } else if(target.classList.contains('time-block')){
+            let cellBox = targetCell.getBoundingClientRect();
+
+            // diff between mouseY and the cellY
+            let yDiff = e.clientY - cellBox.top;
+            // round to the nearest 15 minutes
+            let height = snapNum(yDiff, pxPer15Mins);
+
+            if(height < 0){
+                let blockY = parseInt(cellBox.y);
+                newBlock.style.top = (blockY + height) + 'px';
+                newBlock.style.height = Math.abs(height) + 'px';
+            } else {
+                newBlock.style.height = height + 'px';
+            }
 
         }
 
