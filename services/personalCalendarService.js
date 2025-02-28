3d5525ea81e5b715c21e8add0b4aa5be9298340d
hayde26904 <56613295+hayde26904@users.noreply.github.com>
2025-02-28 12:22:33 -0500

    starting personal calendar backend

---
+++ services/personalCalendarService.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const sql = require('sqlite3').verbose();
+const db = require('../util/dbAsyncWrapper');
+
+async function getUserCalendar(userUID){
+    let dates = await db.all('SELECT * FROM calendar_dates WHERE user_uid = ?;', [userUID]);
+    let datesMap = new Map();
+
+    dates.forEach(date => {
+        if (datesMap.has(date.date)) {
+            datesMap.get(date.date).push([date.start, date.end]);
+        } else {
+            datesMap.set(date.date, [[date.start, date.end]]);
+        }
+    });
+
+    return datesMap;
+};
+
+async function saveUserCalendar(userUID, newDatesMap){
+    let currentDatesMap = await getUserCalendarCalendar(userUID);
+
+    Array.from(newDatesMap).forEach(async ([date, busyTimes]) => {
+        if (currentDatesMap.has(date)) {
+
+            busyTimes.forEach(async ([start, end], index) => {
+                
+            });
+
+        } else {
+            
+        }
+    });
+
+}
