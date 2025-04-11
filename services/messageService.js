a775577df4fdcd1bfefeb1090643af3597038645
aRobert <124544863+RobertFilms@users.noreply.github.com>
2025-04-11 11:01:55 -0400

    fixed a bug

--- services/messageService.js
+++ services/messageService.js
@@ -1,12 +1,8 @@
 const db = require('../util/dbAsyncWrapper');
-const calenderService = require('./personalCalendarService');
-const dateRanker = require('./rankDates');
-const DaySchedule = require('./DaySchedule');
-const dayjs = require('dayjs');
 
 async function addBoard(event, name) {
-    const sql = 'INSERT INTO boards (event_uid, name) VALUES (?, ?)';
-    return await db.run(sql, [event, board]);
+    const sql = 'INSERT INTO message_boards (event_uid, title) VALUES (?, ?)';
+    return await db.run(sql, [event, name]);
 }
 
 module.exports = {
